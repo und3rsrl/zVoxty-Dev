33901827aa314b01577e327c8bd2512143402bc0
zVoxty <zvoxty@gmail.com>
2016-05-11 21:40:25 +0300

    Add files via upload

---
+++ CodeForces/Code_forces#352_1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int a[1000];
+    int b[4000];
+    int j = 1;
+    int l = 10;
+    int k = 11;
+    int z = 192;
+    int nr;
+
+
+    for(int i = 1; i <= 1000; i++){
+        a[i] = i;
+    }
+    for(int i = 1; i <= 100; i++){
+        cout << a[i] << " ";
+    }
+
+    cout << "\n\n\n";
+
+
+    while(j <= 1000){
+        if(j < 10){
+            b[j] = a[j];
+            j++;
+        }
+
+        if(j >= 10 && j < 100){
+
+                b[l] = a[j]/10;
+                b[k] = a[j]%10;
+                j++;
+                l += 2;
+                k += 2;
+        }
+
+        if(j >=100 && j <= 1000){
+
+            b[l] = a[j]/100;
+            b[k] = a[j]/10%10;
+            b[z] = a[j]%100%10;
+            j++;
+            l += 3;
+            k += 3;
+            z += 3;
+
+        }
+    }
+
+    cin >> nr;
+    cout << b[nr];
+
+    return 0;
+}
